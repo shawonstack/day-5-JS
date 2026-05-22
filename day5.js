d6fe9424ac99b4d723b256cb80872aee5b01da43
shawonstack <shawon934764@gmail.com>
2026-05-22 18:19:19 +0600

    for loop outside variable releted issue done

--- day5.js
+++ day5.js
@@ -1,1 +1,25 @@
 console.log('lts dive into loop');
+let name = 'Bangladesh';
+for (let i = 1; i <= 10; i++) {
+  console.log(name);
+}
+
+const product = 'watch';
+for (let i = 1; i <= 10; i++) {
+  console.log(name, product);
+}
+
+const apple = 'apple';
+const watch = 'watch';
+const price = 25000;
+for (let i = 1; i <= 10; i++) {
+  console.log(
+    'this product name is :',
+    apple,
+    'and it is :',
+    watch,
+    'brand.',
+    'its stars only ',
+    price,
+  );
+}
