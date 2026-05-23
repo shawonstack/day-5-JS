175c936fbd5f81bb5a020b3328cb1aef1a77a48d
shawonstack <shawon934764@gmail.com>
2026-05-23 12:07:59 +0600

    lopps

--- day5.js
+++ day5.js
@@ -1,25 +1,50 @@
-console.log('lts dive into loop');
-let name = 'Bangladesh';
+// // console.log('lts dive into loop');
+// let name = 'Bangladesh';
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(name);
+// }
+
+// const product = 'watch';
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(name, product);
+// }
+
+// const apple = 'apple';
+// const watch = 'watch';
+// const price = 25000;
+// for (let i = 1; i <= 10; i++) {
+//   // console.log(
+//     'this product name is :',
+//     apple,
+//     'and it is :',
+//     watch,
+//     'brand.',
+//     'its stars only ',
+//     price,
+//   // );
+// }
+
+let country = 'usa';
 for (let i = 1; i <= 10; i++) {
-  console.log(name);
+  if (i % 2 == 0) {
+    // console.log(country);
+  }
+}
+let numbers = 0;
+while (numbers <= 10) {
+  // console.log('number is :', numbers);
+  numbers++;
 }
 
-const product = 'watch';
-for (let i = 1; i <= 10; i++) {
-  console.log(name, product);
-}
+// DO while loop
+let foodPrice = 1;
+do {
+  console.log('*');
+  foodPrice++;
+} while (foodPrice <= 10);
 
-const apple = 'apple';
-const watch = 'watch';
-const price = 25000;
-for (let i = 1; i <= 10; i++) {
-  console.log(
-    'this product name is :',
-    apple,
-    'and it is :',
-    watch,
-    'brand.',
-    'its stars only ',
-    price,
-  );
+// for off loop
+
+for (let h = 1; h <= 10; h -= -1) {
+  console.log(h);
 }
