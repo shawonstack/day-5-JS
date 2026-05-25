25227848aa09e3b12246fae86666c605385b1f55
shawonstack <shawon934764@gmail.com>
2026-05-25 11:28:47 +0600

    added

--- day5.js
+++ day5.js
@@ -1,69 +1,75 @@
-// // console.log('lts dive into loop');
-// let name = 'Bangladesh';
+// // // console.log('lts dive into loop');
+// // let name = 'Bangladesh';
+// // for (let i = 1; i <= 10; i++) {
+// //   // console.log(name);
+// // }
+
+// // const product = 'watch';
+// // for (let i = 1; i <= 10; i++) {
+// //   // console.log(name, product);
+// // }
+
+// // const apple = 'apple';
+// // const watch = 'watch';
+// // const price = 25000;
+// // for (let i = 1; i <= 10; i++) {
+// //   // console.log(
+// //     'this product name is :',
+// //     apple,
+// //     'and it is :',
+// //     watch,
+// //     'brand.',
+// //     'its stars only ',
+// //     price,
+// //   // );
+// // }
+
+// let country = 'usa';
 // for (let i = 1; i <= 10; i++) {
-//   // console.log(name);
+//   if (i % 2 == 0) {
+//     // console.log(country);
+//   }
+// }
+// let numbers = 0;
+// while (numbers <= 10) {
+//   // console.log('number is :', numbers);
+//   numbers++;
 // }
 
-// const product = 'watch';
-// for (let i = 1; i <= 10; i++) {
-//   // console.log(name, product);
+// // DO while loop
+// let foodPrice = 1;
+// do {
+//   // console.log('*');
+//   foodPrice++;
+// } while (foodPrice <= 10);
+
+// // for off loop
+
+// for (let h = 1; h <= 10; h -= -1) {
+//   // console.log(h);
 // }
 
-// const apple = 'apple';
-// const watch = 'watch';
-// const price = 25000;
-// for (let i = 1; i <= 10; i++) {
-//   // console.log(
-//     'this product name is :',
-//     apple,
-//     'and it is :',
-//     watch,
-//     'brand.',
-//     'its stars only ',
-//     price,
-//   // );
+// let arrays = ['shawon', 'hasan', 'jakir', 'suvo', 'nabil', 'jihad', 'nayeem'];
+// for (let i = 0; i < arrays.length; i++) {
+//   // console.log(arrays[i]);
+// }
+// let array = ['shawon', 'hasan', 'jakir', 'suvo', 'nabil', 'jihad', 'nayeem'];
+
+// for (let i = arrays.length - 1; i >= 0; i--) {
+//   // console.log(array[i]);
 // }
 
-let country = 'usa';
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 == 0) {
-    // console.log(country);
-  }
-}
-let numbers = 0;
-while (numbers <= 10) {
-  // console.log('number is :', numbers);
-  numbers++;
-}
+// const collectors = [12, 2, 3, 4, 56, 7, 2];
+// for (const colelct of collectors) {
+//   // console.log(colelct);
+// }
+// const totalArray = ['5', '7', '9', '12', '34', '77'];
+// for (const total of totalArray) {
+//   console.log(total);
+// }
 
-// DO while loop
-let foodPrice = 1;
+let shawon = 1;
 do {
-  // console.log('*');
-  foodPrice++;
-} while (foodPrice <= 10);
-
-// for off loop
-
-for (let h = 1; h <= 10; h -= -1) {
-  // console.log(h);
-}
-
-let arrays = ['shawon', 'hasan', 'jakir', 'suvo', 'nabil', 'jihad', 'nayeem'];
-for (let i = 0; i < arrays.length; i++) {
-  // console.log(arrays[i]);
-}
-let array = ['shawon', 'hasan', 'jakir', 'suvo', 'nabil', 'jihad', 'nayeem'];
-
-for (let i = arrays.length - 1; i >= 0; i--) {
-  // console.log(array[i]);
-}
-
-const collectors = [12, 2, 3, 4, 56, 7, 2];
-for (const colelct of collectors) {
-  // console.log(colelct);
-}
-const totalArray = ['5', '7', '9', '12', '34', '77'];
-for (const total of totalArray) {
-  console.log(total);
-}
+  console.log('*');
+  shawon++;
+} while (shawon <= 10);
