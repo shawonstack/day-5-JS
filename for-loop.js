dcdffc630ddcb2cf26a93e3a9d52c923d32b7338
shawonstack <shawon934764@gmail.com>
2026-05-26 16:50:15 +0600

    for loops more perameter use n console log

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+// for loop basic  structre is: for(initialization:conditin: increment/decrement)
+for (let i = 1; i <= 10; i++) {
+  // console.log('the increment number is:', i);
+}
+//  2nd is odd number print using for loop
+for (let a = 1; a <= 10; a++) {
+  // console.log(a);
+}
+for (let b = 1; b <= 10; b++) {
+  // console.log(b);
+}
+for (let c = 3; c <= 10; c += 2) {
+  // console.log(c);
+}
+for (let d = 10; d >= 1; d -= 2) {
+  //   console.log(d);
+}
+
+for (let i = 1; i <= 3; i++) {
+  for (let j = 1; j <= 2; j++) {
+    // console.log(i, j);
+  }
+}
+for (let i = 0, j = 10; i <= 10; i++, j--) {
+  console.log(i, j);
+}
