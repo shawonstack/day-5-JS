6330afaa9e19de379b75a1bf438fa948ac2d5861
shawonstack <shawon934764@gmail.com>
2026-05-27 12:29:29 +0600

    gaming hadlth  detecton

--- do-while.js
+++ do-while.js
@@ -1,5 +1,24 @@
-let i = 1;
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 5);
+// let i = 1;
+
+// do {
+//   console.log('the number of reerce is :', i);
+//   i = i + 2;
+// } while (i <= 20);
+// let input = '';
+// let password = '1234';
+
+// do {
+//   input = prompt('Enter your password');
+// } while (input !== password);
+
+// console.log('Login Success');
+let health = 3;
 do {
-  console.log(i);
-  i++;
-} while (i < 5);
+  console.log('health remaining:', health);
+  health--;
+} while (health > 0);
