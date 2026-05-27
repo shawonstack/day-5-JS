c611a70f319174d695e98e7f6808de58721913cd
shawonstack <shawon934764@gmail.com>
2026-05-27 17:53:49 +0600

    for of loops initail starting using bunch of array and cart

---
+++ for-of.js
@@ -0,0 +1,14 @@
+const movies = ['Dark', 'Money Heist', 'Breaking Bad'];
+for (const move of movies) {
+  console.log(move);
+}
+const cart = [1200, 300, 500];
+let total = 0;
+for (const price of cart) {
+  total += price;
+  console.log('prie of the cart is:', total);
+}
+const users = ['Hasan', 'Rahim', 'Karim'];
+for (const user of users) {
+  console.log(`notification sent to ${user}`);
+}
