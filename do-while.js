6e5dae29ae0e97d9560aee1d4a973def05ac70b4
shawonstack <shawon934764@gmail.com>
2026-05-27 10:59:40 +0600

    practice groupn task 1

--- do-while.js
+++ do-while.js
@@ -1,15 +1,50 @@
-// While Loop — “Condition jotokkhon true, totokkhon cholbe”
-while (condition) {
-  // code
-}
+// // // While Loop — “Condition jotokkhon true, totokkhon cholbe”
+// // while (condition) {
+// //   // code
+// // }
 
-// let i = 1;
-while (i <= 5) {
-  // console.log(i);
+// // // let i = 1;
+// // while (i <= 5) {
+// //   // console.log(i);
+// //   i++;
+// // }
+// // let i = 10;
+// // while (i >= 1) {
+// //   console.log(i);
+// //   i--;
+// // }
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 2;
+// }
+// -----------------
+// usr contrled loop
+// let password = '1234';
+// let input = '';
+
+// while (input !== password) {
+//   input = prompt('Enter Password');
+// }
+// ------------------------------array travarsal using while
+// let foods = ['apple', 'banana', 'lichy', 'mango'];
+// let i = 0;
+// while (i < foods.length) {
+//   console.log(foods[i]);
+//   i++;
+// }
+
+// -----------------OTP retry system
+// let otp = 1234;
+// let input = '';
+// let attempt = 0;
+// while (input !== otp && attempt < 3) {
+//   console.log('wrong otp. try correct one. 2 more remaining');
+//   attempt++;
+// }
+//-----------------------task1
+let i = 1;
+while (i <= 20) {
+  console.log('number of i is: ', i);
   i++;
-}
-let i = 10;
-while (i >= 1) {
-  console.log(i);
-  i--;
 }
