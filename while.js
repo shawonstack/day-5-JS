0e6cf0e0306547c1a69c6c0c8400435c55d67e71
shawonstack <shawon934764@gmail.com>
2026-05-27 12:16:12 +0600

    do.. while setup initailly

---
+++ while.js
@@ -0,0 +1,52 @@
+// // // While Loop — “Condition jotokkhon true, totokkhon cholbe”
+// // while (condition) {
+// //   // code
+// // }
+
+// // // let i = 1;
+// // while (i <= 5) {
+// //   // console.log(i);
+// //   i++;
+// // }
+// // let i = 10;
+// // while (i >= 1) {
+// //   console.log(i);
+// //   i--;
+// // }
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i = i + 2;
+// }
+// -----------------
+// usr contrled loop
+// let password = '1234';
+// let input = '';
+
+// while (input !== password) {
+//   input = prompt('Enter Password');
+// }
+// ------------------------------array travarsal using while
+// let foods = ['apple', 'banana', 'lichy', 'mango'];
+// let i = 0;
+// while (i < foods.length) {
+//   console.log(foods[i]);
+//   i++;
+// }
+
+// -----------------OTP retry system
+// let otp = 1234;
+// let input = '';
+// let attempt = 0;
+// while (input !== otp && attempt < 3) {
+//   console.log('wrong otp. try correct one. 2 more remaining');
+//   attempt++;
+// }
+//-----------------------task1
+let i = 1;
+let sum = 0;
+while (i <= 10) {
+  sum = sum + i;
+  i++;
+  console.log(sum);
+}
