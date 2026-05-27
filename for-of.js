7ae329bc28e4171fae8ab68386cb8ac988a4c87a
shawonstack <shawon934764@gmail.com>
2026-05-27 18:01:32 +0600

    break and if else ststements used in nested for of loops

--- for-of.js
+++ for-of.js
@@ -1,14 +1,43 @@
-const movies = ['Dark', 'Money Heist', 'Breaking Bad'];
-for (const move of movies) {
-  console.log(move);
+// const movies = ['Dark', 'Money Heist', 'Breaking Bad'];
+// for (const move of movies) {
+//   console.log(move);
+// }
+// const cart = [1200, 300, 500];
+// let total = 0;
+// for (const price of cart) {
+//   total += price;
+//   console.log('prie of the cart is:', total);
+// }
+// const users = ['Hasan', 'Rahim', 'Karim'];
+// for (const user of users) {
+//   console.log(`notification sent to ${user}`);
+// }
+//api data processing
+// const products = [
+//   { name: 'Mouse', price: 500 },
+//   { name: 'Keyboard', price: 1000 },
+// ];
+// for (const product of products) {
+//   console.log(
+//     `name o the product is ${product.name} and price is ${product.price}`,
+//   );
+// }
+
+const groups = [
+  ['Rahim', 'Karim'],
+  ['Hasan', 'Jahid'],
+];
+for (const team of groups) {
+  for (const members of team) {
+    console.log(members);
+  }
 }
-const cart = [1200, 300, 500];
-let total = 0;
-for (const price of cart) {
-  total += price;
-  console.log('prie of the cart is:', total);
-}
-const users = ['Hasan', 'Rahim', 'Karim'];
-for (const user of users) {
-  console.log(`notification sent to ${user}`);
+const numbers = [1, 2, 3, 4, 5];
+
+for (const num of numbers) {
+  if (num === 3) {
+    break;
+  }
+
+  console.log(num);
 }
