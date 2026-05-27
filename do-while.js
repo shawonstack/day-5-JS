341a96c27a96e2f2797b9f133084fe524101843e
shawonstack <shawon934764@gmail.com>
2026-05-27 10:33:44 +0600

    while lopping

---
+++ do-while.js
@@ -0,0 +1,15 @@
+// While Loop — “Condition jotokkhon true, totokkhon cholbe”
+while (condition) {
+  // code
+}
+
+// let i = 1;
+while (i <= 5) {
+  // console.log(i);
+  i++;
+}
+let i = 10;
+while (i >= 1) {
+  console.log(i);
+  i--;
+}
